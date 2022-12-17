3cff8378530da67f49c167b6ad6d6f0882240a7a
Hgencer3 <onlygencer@gmail.com>
2022-12-17 10:02:31 -0500

    Swagger

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -4,7 +4,6 @@ import com.cydeo.dto.ProjectDTO;
 import com.cydeo.dto.TaskDTO;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.entity.User;
-import com.cydeo.exception.TicketingProjectException;
 import com.cydeo.mapper.UserMapper;
 import com.cydeo.repository.UserRepository;
 import com.cydeo.service.KeycloakService;
@@ -70,7 +69,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserDTO update(UserDTO dto) {
 
-        //Find current user
+       //Find current user
         User user = userRepository.findByUserName(dto.getUserName());
         //Map updated user dto to entity object
         User convertedUser = userMapper.convertToEntity(dto);
@@ -89,7 +88,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public void delete(String username) throws TicketingProjectException {
+    public void delete(String username) {
         User user = userRepository.findByUserName(username);
 
         if (checkIfUserCanBeDeleted(user)) {
@@ -97,17 +96,11 @@ public class UserServiceImpl implements UserService {
             user.setUserName(user.getUserName() + "-" + user.getId());
             userRepository.save(user);
             keycloakService.delete(username);
-        } else {
-            throw new TicketingProjectException("User can not be deleted");
         }
 
     }
 
-    private boolean checkIfUserCanBeDeleted(User user) throws TicketingProjectException {
-
-        if (user == null) {
-            throw new TicketingProjectException("User not found");
-        }
+    private boolean checkIfUserCanBeDeleted(User user) {
 
         switch (user.getRole().getDescription()) {
             case "Manager":
