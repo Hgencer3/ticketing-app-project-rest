ae93255ad278ad64ef8c22e279f47596db12ebab
Hgencer3 <onlygencer@gmail.com>
2022-12-17 09:43:17 -0500

    Initial commit

---
+++ src/main/java/com/example/ticketingappprojectrest/TicketingAppProjectRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticketingappprojectrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingAppProjectRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingAppProjectRestApplication.class, args);
+    }
+
+}
